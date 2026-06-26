bfcb5d3af43e87a995ac736b69aa70e38ad92db6
Joe Orchard <joe.orchard03@gmail.com>
2026-06-26 16:00:55 +0000

    More testing and adding question page

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require('express')
+const express = require('express') // Loads the express framework
 const app = express() //Creates the app
-
-app.listen(3000) // Starts server
 
 //When someone visits the home page
 //req is what the user asked for, res is what you send back to the user
 app.get('/', (req, res) => { 
-  res.send('Home page') //res.send is used to send text back to the browser
+  res.send('Driving test revision app: Home Page') //res.send is used to send text back to the browser
 })
 
 //Loads About page text when they go to about page
 app.get('/about', (req, res) => {
-  res.send('About page')
+  res.send('This is a driving test revision app that helps people prepare for their driving test')
 })
 
-//Loads Hello JSON when going to api/test page
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Hello JSON' })
-})
+//Loads questions page
+app.get('/questions', (req, res) => {
+  res.json([
+      {
+      question: "What does this sign mean?",
+      answer: "Stop"
+      },
+      {
+      question: "What is the speed limit on a dual carriageway in Wales?",
+      answer: "70mph"
+      }
+    ]
+  )})
+
+
+app.listen(3000) // Starts server
